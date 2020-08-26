1e6f5e71113d8eca49b95a55a61c049e52a236af
Joao Vitor <joaovitorsilvaluciano@gmail.com>
2020-08-26 01:40:50 -0300

    gh-pages

---
+++ precache-manifest.73a5af3c6475309a0587015cb755e418.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d92c2a2d75e7f005c6f38d7d947224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de1e3d1a1bf148d53db1",
+    "url": "/static/css/main.f216c0b1.chunk.css"
+  },
+  {
+    "revision": "e106e4057f4d756a0bc7",
+    "url": "/static/js/2.33eda02d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.33eda02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1e3d1a1bf148d53db1",
+    "url": "/static/js/main.bcd7b0d6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a0ce30a57ae469a4f3a7cd794f78b905",
+    "url": "/static/media/2DS.a0ce30a5.png"
+  },
+  {
+    "revision": "a3a06a803e55ffae9fea8717aa52a7ee",
+    "url": "/static/media/filme.a3a06a80.png"
+  },
+  {
+    "revision": "358fe77529ac55ac000bdcd4cabd2665",
+    "url": "/static/media/games.358fe775.png"
+  },
+  {
+    "revision": "2b04146955529b0c369692200548f14e",
+    "url": "/static/media/instrumentos.2b041469.png"
+  },
+  {
+    "revision": "0d52a7be8c4e4fad3edfa8aa32dc0711",
+    "url": "/static/media/joao.0d52a7be.jpg"
+  },
+  {
+    "revision": "1e6b00ceadbfd695cb5904cd32793f20",
+    "url": "/static/media/obg.1e6b00ce.png"
+  },
+  {
+    "revision": "20344985ad6a0267bd6c2b5eb643b0ce",
+    "url": "/static/media/opi.20344985.png"
+  },
+  {
+    "revision": "8d43c71f9447f017a29fc7b0b5c20a1d",
+    "url": "/static/media/tjr.8d43c71f.png"
+  }
+]);
